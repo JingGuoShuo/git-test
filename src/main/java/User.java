f9ca22436b8dfa218cc8d8d90708b7b1d8fdc879
JingGuoShuo <571549353@qq.com>
2018-03-09 16:12:54 +0800

    My First Commit

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -1,5 +1,19 @@
 public class User {
     private Integer id;
+    private String name;
+
+    public User(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
 
     public User() {
     }
